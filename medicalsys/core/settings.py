c3e746fffa3220b19f649ff96a01ec86abad62c0
Victor Azevedo <josevcazevedo@gmail.com>
2023-07-24 17:09:59 -0300

    chore: language and time_zone update

--- medicalsys/core/settings.py
+++ medicalsys/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # Local apps
     'users.apps.UsersConfig',
     'patients.apps.PatientsConfig',
+    'schedule.apps.ScheduleConfig',
 ]
 
 MIDDLEWARE = [
@@ -111,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Recife'
 
 USE_I18N = True
 
